0d2895d9f19b5dfbcfc45587a174554fe00185e4
Madiyar <madiyaresmuhambetov7@gmail.com>
2020-12-22 14:06:59 +0600

    in overlay add social links

--- js/bundle.js
+++ js/bundle.js
@@ -11,15 +11,18 @@ function overlayy (){
     let btn = document.querySelector('.btn_menu'),
         ovr = document.querySelector(".overlay"),
         cls = document.querySelector('.overlay__close'),
+        soc = document.querySelector(".overlay__social"),
         menu = document.querySelector(".overlay__menu");
     btn.addEventListener('click', ()=>{
         ovr.classList.add("overlay__show");
         menu.classList.add("overlay__menu_show");
+        soc.classList.add("overlay__social_show");
         document.body.style.overflow = 'hidden';
     });  
     cls.addEventListener('click', ()=>{
         ovr.classList.remove("overlay__show");
         menu.classList.remove("overlay__menu_show");
+        soc.classList.remove("overlay__social_show");
         document.body.style.overflow = '';
     });   
 
