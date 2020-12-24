3f9fbd68a50808ef755938db55294b84baec0550
Madiyar <madiyaresmuhambetov7@gmail.com>
2020-12-24 13:08:34 +0600

    add links

--- js/bundle.js
+++ js/bundle.js
@@ -10,13 +10,7 @@
 function adv (){
     let blc = document.querySelectorAll(".advantages__content");
     window.addEventListener('scroll', function(){
-            // let a = blc[0].getBoundingClientRect().top;
-            // let b = blc[1].getBoundingClientRect().top;
-            // let c = blc[2].getBoundingClientRect().top;
-            // let d = blc[3].getBoundingClientRect().top;
-            // if(blc < 0){
-            //     blc.forEach((item)=> item.classList.add('easeIn'));
-            // }
+       
             let c = document.documentElement.clientHeight; 
             for(let i = 0; i < blc.length; i++){
                let b = blc[i].getBoundingClientRect().top - c;
@@ -44,7 +38,9 @@ function overlayy (){
         ovr = document.querySelector(".overlay"),
         cls = document.querySelector('.overlay__close'),
         soc = document.querySelector(".overlay__social"),
+        href = document.querySelectorAll('.promo__item'),
         menu = document.querySelector(".overlay__menu");
+        
     btn.addEventListener('click', ()=>{
         ovr.classList.add("overlay__show");
         ovr.addEventListener("transitionend", ()=>{
@@ -53,7 +49,19 @@ function overlayy (){
             document.body.style.overflow = 'hidden';
         });
     });  
-    
+    href.forEach((item)=> item.addEventListener('click', ()=>{
+        menu.classList.remove("overlay__menu_show");
+        soc.classList.remove("overlay__social_show");
+        ovr.classList.remove("overlay__show");
+        ovr.addEventListener("transitionend", ()=>{
+        
+        menu.classList.remove("overlay__menu_show");
+        soc.classList.remove("overlay__social_show");
+
+        document.body.style.overflow = '';
+       }); 
+        
+    }) );
     cls.addEventListener('click', ()=>{
         menu.classList.remove("overlay__menu_show");
         soc.classList.remove("overlay__social_show");
--- js/parts/advantages.js
+++ js/parts/advantages.js
@@ -1,13 +1,7 @@
 function adv (){
     let blc = document.querySelectorAll(".advantages__content");
     window.addEventListener('scroll', function(){
-            // let a = blc[0].getBoundingClientRect().top;
-            // let b = blc[1].getBoundingClientRect().top;
-            // let c = blc[2].getBoundingClientRect().top;
-            // let d = blc[3].getBoundingClientRect().top;
-            // if(blc < 0){
-            //     blc.forEach((item)=> item.classList.add('easeIn'));
-            // }
+       
             let c = document.documentElement.clientHeight; 
             for(let i = 0; i < blc.length; i++){
                let b = blc[i].getBoundingClientRect().top - c;
